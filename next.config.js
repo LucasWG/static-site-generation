ac137c0767651b1491f82c7d48a165da2ababde5
LucasWG94 <LucasWG94@gmail.com>
2020-11-21 15:29:36 -0300

    :rocket: added next/image and next.config.js

---
+++ next.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	images: {
+		domains: [
+			'avatars0.githubusercontent.com',
+			'avatars1.githubusercontent.com',
+			'avatars2.githubusercontent.com',
+			'avatars3.githubusercontent.com',
+		],
+		i18n: {
+			locales: ['pt', 'en'],
+			defaultLocale: 'pt'
+		}
+	}
+}
--- pages/users/[login].tsx
+++ pages/users/[login].tsx
@@ -1,54 +1,61 @@
 import { GetStaticProps, GetStaticPaths } from 'next'
 import { useRouter } from 'next/router'
+import Image from 'next/image'
 
 export default function Users({ user }) {
-    const { isFallback } = useRouter()
+	const { isFallback } = useRouter()
 
-    if (isFallback)
-        return <p>Carregando...</p>
+	if (isFallback)
+		return <p>Carregando...</p>
 
-    return (
-        <>
-            <img src={user.avatar_url} alt={user.name} width="80" style={{ borderRadius: 40 }} />
-            <h1>{user.name}</h1>
-            <p>{user.bio}</p>
-        </>
-    )
+	return (
+		<>
+			{user.avatar_url ? (
+				<>
+					<Image src={user.avatar_url} width="400" height="400" alt={user.name} />
+					<h1>{user.name}</h1>
+					<p>{user.bio}</p>
+				</>
+			) : (
+					<h1>API rate limit exceeded</h1>
+				)}
+		</>
+	)
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
-    const response = await fetch(`https://api.github.com/orgs/rocketseat/members`)
-    const data = await response.json()
+	const response = await fetch(`https://api.github.com/orgs/rocketseat/members`)
+	const data = await response.json()
 
-    if (!Array.isArray(data)) {
-        return {
-            paths: [],
-            fallback: true
-        }
-    }
+	if (!Array.isArray(data)) {
+		return {
+			paths: [],
+			fallback: true
+		}
+	}
 
-    const paths = data.map(member => {
-        return { params: { login: member.login } }
-    })
+	const paths = data.map(member => {
+		return { params: { login: member.login } }
+	})
 
-    return {
-        paths,
-        fallback: true
-    }
+	return {
+		paths,
+		fallback: true
+	}
 }
 
 export const getStaticProps: GetStaticProps = async (context) => {
-    const { login } = context.params
-    const response = await fetch(`https://api.github.com/users/${login}`)
-    const data = await response.json()
+	const { login } = context.params
+	const response = await fetch(`https://api.github.com/users/${login}`)
+	const data = await response.json()
 
-    if (!data)
-        return { notFound: true }
+	if (!data)
+		return { notFound: true }
 
-    return {
-        props: {
-            user: data
-        },
-        revalidate: 60
-    }
+	return {
+		props: {
+			user: data
+		},
+		revalidate: 60
+	}
 }
